5e6c7f7960f3f87a53eedc58512c34c31aa22ef6
Maleeha Nasir <mk09991@st.habib.edu.pk>
2026-02-19 00:42:56 +0500

    lab4

--- Lab 04/Task 2.s
+++ Lab 04/Task 2.s
@@ -1,16 +1,47 @@
 .text
-.globl main
-ntri:
-    li x10, 1 #i=0
-    li x11, 0 #sum start
-    li x12, 6 #end of loop loop runs x12's value -1 times
+.globl 
 
-loop1:
-    beq x10, x12, exit
-    add x11, x11, x10
-    addi x10, x10, 1
-    j loop1
+main:
+    addi x10, x0, 3
+    jal x1, ntri
+
+    addi x11, x10, 0
+    li x10, 1
+    ecall 
+    j exit
+
+ntri:
+    addi sp, sp, -8
+
+    sw x1, 4(sp)
+    sw x10, 0(sp)
+
+    #base case check
+    addi x5, x10, -1 #x5 = num - 1 
+    blt x10, x0, ntri_base
+
+
+ntri_recursive:
+    addi x10, x10, -1      
+    jal x1, ntri #recursive call       
+    addi x6, x10, 0
+
+    lw x10, 0(sp)
+    lw x1, 4(sp)         
+    addi sp, sp, 8
+    
+    add x10, x10, x6
+    jalr x0, 0(x1)
+
+ntri_base:
+    addi x10, x0, 0
+    addi sp, sp, 4
+    jalr x0, 0(x1)
 
 exit:
     j exit
+
+    
+
+
 
--- Lab 04/Task 3.s
+++ Lab 04/Task 3.s
@@ -1,42 +1,62 @@
 .text
-.globl main
+.globl
+
 main:
-    li x10, 0(x100) #array base address
+    li x10, 0x200
+    #populate the array
+    li x5, 8
+    sw x5, 0(x10)
+    li x5, 20
+    sw x5, 4(x10)
+    li x5, 15
+    sw x5, 8(x10)
+    li x5, 3
+    sw x5, 12(x10)
+    li x5, 9
+    sw x5, 16(x10)
 
-    li x6, 23
-    sw x6, 0(x1)
+    jal x1, bubble_sort #call bubble sort
 
-    li x6, 12
-    sw x6, 4(x1)
+bubble_sort:
+    #allocatian of stack
+    addi sp, sp, -8
+    sw x8,8(sp)
+    sw x9, 4(sp)
 
-    li x6, 5
-    sw x6, 8(x1)
+    #bubble sort Loqic
+    li x7, 0 #i
+    li x9, 4
 
-    li x6, 44
-    sw x6, 12(x1)
+outer_loop:
+    bge x7, x9, exit
+    li x8, 0 #j=0
+    li x6, 0 #swapped = false
+    sub x9, x9, x7 #Limit of j = n-i-1
 
-    li x6, 98
-    sw x6, 16(x1)
+inner_loop:
+    bge x8, x9, check_flag
 
+    # Load a[j] and a[j+1]
+    slli x5, x8, 2
+    add x5, x10, x5
+    lw x20, 0(x5)
+    lw x21, 4(x5)
 
-    li x6, 5 #initialize size of array
-    li x7, 0 #i=0
+    ble x28, x21, no_swap
 
-loop1:
-    beq x7, x6, exit
-    add x10, x7, x0 #j=i
-    addi x7, x7, 1 #i++
-    add 121, x7, x0 #loaded i into x10
-    addi x10, x10, 3 #incremented it to get arr ofset size and all
-    #so here x10 has arr[i]
-    
-loop2:
-    beq x10, x6, loop1
-    lw x11, 0(x1) #arr[i]
-    addi x1, x1, 4 #increment 
-    lw x12, 0(x1) #arr[j]
+    # swap
+    sw x21, 0(x5)
+    sw x20, 4(x5)
+    li x6, 1
 
-    
+no_swap:
+    addi x8, x8, 1
+    j inner_loop
 
+check_flag:
+    beq x6, x0, exit
+    addi x7, x7, 1
+    j outer_loop
 
-
+exit:
+    j exit
